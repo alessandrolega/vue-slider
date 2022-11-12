4bf0f09aa7898fc6fe42245f8033e3eb740700c4
alessandrolega <alessandrolega24@gmail.com>
2022-11-12 22:13:22 +0100

    aggiunto array di oggetti images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 var app = new Vue({
     el: '#root',
     data: {
-      title: 'Hello Vue!'
+      images: [
+        { id: 1, image: '../img/amsterdam.jpg'},
+        { id: 2, image: '../img/barcellona.jpg'},
+        { id: 3, image: '../img/londra.jpg'},
+        { id: 4, image: '../img/lviv.jpg'},
+        { id: 5, image: '../img/madrid.jpg'},
+        { id: 6, image: '../img/roma.jpg'}
+      ]
+      
     }
   })
+
+
+  
+
+ 
