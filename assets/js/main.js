15097ff3d484c124082000671badcac15fd72f01
alessandrolega <alessandrolega24@gmail.com>
2022-11-13 18:26:14 +0100

    aggiunto array  di oggetti "images"

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ var app = new Vue({
     el: '#root',
     data: {
       images: [
-        { id: 1, image: '../img/amsterdam.jpg'},
-        { id: 2, image: '../img/barcellona.jpg'},
-        { id: 3, image: '../img/londra.jpg'},
-        { id: 4, image: '../img/lviv.jpg'},
-        { id: 5, image: '../img/madrid.jpg'},
-        { id: 6, image: '../img/roma.jpg'}
+        
+        {img: 'https://cdn.pixabay.com/photo/2016/01/19/19/26/amsterdam-1150319__480.jpg'},
+        {img: 'https://st.depositphotos.com/1004348/4641/i/600/depositphotos_46412421-stock-photo-las-ramblas-of-barcelona-aerial.jpg'},
+        {img: 'https://www.luccaindiretta.it/photogallery_new/images/2019/10/londra-viaggio-musei-cultura-lingue-jpg-4120.jpg'},
+        {img: 'https://s.inyourpocket.com/gallery/110549.jpg'},
+        {img: 'https://cdn.theculturetrip.com/wp-content/uploads/2017/02/gran-via-main-shopping-street-in-madrid.jpg'},
+        {img: 'https://static2-viaggi.corriereobjects.it/wp-content/uploads/2018/12/iStock-Roma.jpg?v=300845'} 
       ]
-      
     }
   })
 
